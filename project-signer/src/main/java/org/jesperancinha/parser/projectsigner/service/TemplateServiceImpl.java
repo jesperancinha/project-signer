7f714a8d85ba1995ea51f6dfc0d82e4d94920797
Joao Esperancinha <jofisaes@gmail.com>
2019-09-09 14:40:20 +0200

    QA - Decoupling test for Better Code

--- project-signer/src/main/java/org/jesperancinha/parser/projectsigner/service/TemplateServiceImpl.java
+++ project-signer/src/main/java/org/jesperancinha/parser/projectsigner/service/TemplateServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * A markdown template service to handle markdown texts
@@ -26,18 +25,13 @@ public class TemplateServiceImpl implements TemplateService {
     /**
      * Receives an input markdown text stream nd parses its content to a Paragraphs object see {@link Paragraphs}
      *
-     * @param templateInputStream An input markdown text stream
      * @return A {@link Paragraphs} parsed object
      * @throws IOException Any kind of IO Exception
      */
-    private Paragraphs findAllParagraphs(final InputStream templateInputStream) throws IOException {
-        return TemplateParserHelper.findAllParagraphs(templateInputStream);
-    }
-
     @Override
     public Paragraphs findAllParagraphs() throws IOException {
         final File fileTemplate = optionsService.getProjectSignerOptions().getTemplateLocation().toFile();
         final FileInputStream templateInputStream = new FileInputStream(fileTemplate);
-        return findAllParagraphs(templateInputStream);
+        return TemplateParserHelper.findAllParagraphs(templateInputStream);
     }
 }
