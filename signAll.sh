af0cc954d52259e5eac75c7f04c359b4ed36a5fc
Joao Esperancinha <jofisaes@gmail.com>
2019-10-04 13:29:57 +0200

    Signing up to medium

--- signAll.sh
+++ signAll.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
+if [[ -z "$1" ]]
+then
+    commitMessage="Project Signing - Auto generated commit"
+else
+    commitMessage=$1
+fi
 echo -e '\e[31m'WARNING!!'\e[0m'
 echo -e '\e[32m'Carefull when using this script. It is ONLY meant to be used as a signing off tool \for ALL repos in your root folder \(..\)'\e[0m'
-echo "This will automatically commit EVERYTHING under ALL your repos with commit message \"Project Signing - Auto generated commit\""
+echo -e "This will automatically commit EVERYTHING under ALL your repos with commit message \e[32m\"${commitMessage}\"\e[0m"
 read -p "Are you sure? (Yy/Nn)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    commitMessage=$1
     cd ..
     for item in *; do
         cd "${item}"
         echo "----------------- Signing (committing) ${item} -----------------"
         git add .
-        if [[ -z "${commitMessage}" ]]
+        if [[ -z "$1" ]]
         then
              echo "no commit message. defaulting to auto"
-             git commit -a -m "Project Signing - Auto generated commit"
         else
              echo "commit message ${commitMessage}"
-             git commit -a -m "${commitMessage}"
         fi
+         git commit -a -m "${commitMessage}"
         git push
         cd ..
     done
