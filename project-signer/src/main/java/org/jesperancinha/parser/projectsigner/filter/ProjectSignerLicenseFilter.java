6267c181d0a03afe4ddf957b90a6fc234ff7dd5e
Joao Esperancinha <jofisaes@gmail.com>
2020-06-11 15:11:02 +0200

    Project Signing - Auto generated commit

--- project-signer/src/main/java/org/jesperancinha/parser/projectsigner/filter/ProjectSignerLicenseFilter.java
+++ project-signer/src/main/java/org/jesperancinha/parser/projectsigner/filter/ProjectSignerLicenseFilter.java
@@ -9,6 +9,7 @@ public class ProjectSignerLicenseFilter {
 
     public static final String ISC_LICENSE_MARK = "ISC License";
     public static final String APACHE_LICENSE_MARK = "Apache License";
+    public static final String MIT_LICENSE_MARK = "The MIT License";
 
     public static String getLicense(List<String> rawLicenses, String license) throws Throwable {
         if (isNull(license) || license.contains(APACHE_LICENSE_MARK)) {
@@ -16,6 +17,9 @@ public class ProjectSignerLicenseFilter {
                     .findFirst().orElseThrow((Supplier<Throwable>) () -> new RuntimeException("No license found!"));
         } else if (license.contains(ISC_LICENSE_MARK)) {
             return rawLicenses.stream().filter(lic -> lic.contains(ISC_LICENSE_MARK))
+                    .findFirst().orElseThrow((Supplier<Throwable>) () -> new RuntimeException("No license found!"));
+        }else if (license.contains(MIT_LICENSE_MARK)) {
+            return rawLicenses.stream().filter(lic -> lic.contains(MIT_LICENSE_MARK))
                     .findFirst().orElseThrow((Supplier<Throwable>) () -> new RuntimeException("No license found!"));
         }
         throw new RuntimeException("No license found!");
