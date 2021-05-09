18873ed542b9a80179814884b2382d16f8c320e4
Joao Esperancinha <jofisaes@gmail.com>
2021-05-09 12:00:23 +0200

    ✒️ Full coverage report

--- README.md
+++ README.md
@@ -9,6 +9,8 @@
 [![codebeat badge](https://codebeat.co/badges/bfb0987b-e483-4954-9c3b-24ac488006bd)](https://codebeat.co/projects/github-com-jesperancinha-project-signer-master)
 [![BCH compliance](https://bettercodehub.com/edge/badge/jesperancinha/project-signer?branch=master)](https://bettercodehub.com/)
 [![Build status](https://ci.appveyor.com/api/projects/status/eyx7uhjenc7m6s9j/branch/master?svg=true)](https://ci.appveyor.com/project/jesperancinha/project-signer/branch/master)
+[![Coverage Status](https://coveralls.io/repos/github/jesperancinha/project-signer/badge.svg?branch=master)](https://coveralls.io/github/jesperancinha/project-signer?branch=master)
+[![codecov](https://codecov.io/gh/jesperancinha/project-signer/branch/master/graph/badge.svg?token=ErqcT1G5Tq)](https://codecov.io/gh/jesperancinha/project-signer)
 
 [![GitHub language count](https://img.shields.io/github/languages/count/jesperancinha/project-signer.svg)]()
 [![GitHub top language](https://img.shields.io/github/languages/top/jesperancinha/project-signer.svg)]()
---
+++ project-signer-api/src/test/java/org/jesperancinha/parser/projectsigner/inteface/InterfacesTest.java
@@ -0,0 +1,111 @@
+package org.jesperancinha.parser.projectsigner.inteface;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InterfacesTest {
+
+    @Test
+    public void testAllInterfacesWhenCreatingImplementationThenAllow() {
+        final var fileWriterService = new FileWriterService() {
+            @Override
+            public void exportReadmeFile(Path path, String text) throws IOException {
+
+            }
+        };
+
+        final var finderService = new FinderService() {
+            @Override
+            public void iterateThroughFilesAndFolders(Path rootPath) throws IOException {
+
+            }
+        };
+
+        final var generatorService = new GeneratorService<>() {
+            @Override
+            public void processReadmeFile(Path readmePath, Object allParagraphs) throws IOException {
+
+            }
+
+            @Override
+            public void processLicenseFile(Path licencePath, List<String> license) throws Throwable {
+
+            }
+        };
+        final var mergeService = new MergeService<>() {
+            @Override
+            public String mergeDocumentWithFooterTemplate(String readmeMd, Object footer) {
+                return null;
+            }
+
+            @Override
+            public void writeMergedResult(Path readmePath, String newText) throws IOException {
+
+            }
+        };
+
+        final var optionsService = new OptionsService<>() {
+            @Override
+            public Object processOptions(String[] args) {
+                return null;
+            }
+
+            @Override
+            public Object getProjectSignerOptions() {
+                return null;
+            }
+
+            @Override
+            public Object getCommonNamingParser() {
+                return null;
+            }
+        };
+
+        final var readmeNamingService = new ReadmeNamingService() {
+            @Override
+            public InputStream buildReadmeStream(Path path) throws IOException {
+                return null;
+            }
+        };
+
+        final var readmeService = new ReadmeService<>() {
+            @Override
+            public String readDataSprippedOfTags(InputStream templateInputStream, String... tags) throws IOException {
+                return null;
+            }
+
+            @Override
+            public void exportNewReadme(Path readmePath, InputStream inputStream, Object allParagraphs) throws IOException {
+
+            }
+        };
+
+        final var templateService = new TemplateService<>() {
+            @Override
+            public Object findAllParagraphs() throws IOException {
+                return null;
+            }
+
+            @Override
+            public List<String> readAllLicenses() throws IOException {
+                return null;
+            }
+        };
+
+        assertThat(fileWriterService).isNotNull();
+        assertThat(finderService).isNotNull();
+        assertThat(generatorService).isNotNull();
+        assertThat(mergeService).isNotNull();
+        assertThat(optionsService).isNotNull();
+        assertThat(readmeNamingService).isNotNull();
+        assertThat(readmeService).isNotNull();
+        assertThat(templateService).isNotNull();
+
+    }
+}
