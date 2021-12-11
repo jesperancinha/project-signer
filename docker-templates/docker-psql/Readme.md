06fff5e7077bd3ab5cd95275f2446a7d9c646c3d
Joao Esperancinha <jofisaes@gmail.com>
2021-12-11 13:57:35 +0100

    🐳 Extended multi database docker image template

---
+++ docker-templates/docker-psql/Readme.md
@@ -0,0 +1,6 @@
+# Multiple database setup example
+
+This example is taken from my project [![Generic badge](https://img.shields.io/static/v1.svg?label=GitHub&message=StaCo%20App%20🪙%20&color=informational)](https://github.com/jesperancinha/staco-app).
+
+It provides support to a multidatabase system inspired by Spring Auto-scanning and using Postgres official Docker Hub images.
+
---
+++ docker-templates/docker-psql/init-scripts/multiple/data-staco.sql
@@ -0,0 +1,36 @@
+drop type if exists sta_co_json;
+create type sta_co_json as
+(id varchar(255) ,
+    description varchar (255),
+    "year" varchar (255),
+    "value" varchar (255),
+    currency varchar (255),
+    "type" varchar (255),
+    "diameterMM" varchar (255),
+    "internalDiameterMM" varchar (255),
+    "heightMM" varchar (255),
+    "widthMM" varchar (255));
+
+\set content `cat /docker-entrypoint-initdb.d/stamps_coins.json`
+
+with sta_co_json (doc) as (
+    values
+    (:'content'::json)
+)
+
+insert
+into sta_co (id, description, year, value, currency, type, diameter_mm, internal_diameter_mm, height_mm, width_mm)
+select ROW_NUMBER() OVER (
+    ORDER BY description
+    ),
+       description,
+       "year",
+       "value",
+       "currency",
+       "type",
+       "diameterMM"         as diameter_mm,
+       "internalDiameterMM" as internal_diameter_mm,
+       "heightMM"           as height_mm,
+       "widthMM"            as width_mm
+from sta_co_json l
+         cross join lateral json_populate_recordset(null::sta_co_json, doc);
