95d30591940f1300679f3b32baf0430516f8f4aa
Joao Esperancinha <jofisaes@gmail.com>
2022-09-08 22:31:11 +0200

    Complements the clone script

--- cloneAll.sh
+++ cloneAll.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 
-echo -e '\e[31m'WARNING!!'\e[0m'
-echo -e '\e[32m'Carefull when using this script. It will clone EVERY repo from user $1 into the root directory!'\e[0m'
+echo -e '\e[31mWARNING!!\e[0m'
+echo -e "\e[32mCarefull when using this script. It will clone EVERY repo from user $1 into the root directory!\e[0m"
+echo -e "\e[32mExample usage: ./cloneAll.sh jesperancinha 100 jesperancinhaOrg 100\e[0m"
 read -p "Are you sure? (Yy/Nn)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   user=$1
   pages=$2
   org=$3
   porg=$4
-  repos=$(curl https://api.github.com/users/${user}/repos?per_page=${pages})
+  repos=$(curl https://api.github.com/users/"${user}"/repos?per_page="${pages}")
   cd ..
   for row in $(echo "${repos}" | jq -r '.[] | @base64'); do
     _jq() {
-      echo ${row} | base64 --decode | jq -r ${1}
+      echo "${row}" | base64 --decode | jq -r "${1}"
     }
     if [[ $(_jq '.fork') =~ ^'false'$ ]]; then
       repoAddress=$(_jq '.ssh_url')
-      echo ${repoAddress}
-      git clone ${repoAddress}
+      echo "${repoAddress}"
+      git clone "${repoAddress}"
     fi
   done
-  repos=$(curl https://api.github.com/orgs/${org}/repos?per_page=${porg})
+  repos=$(curl https://api.github.com/orgs/"${org}"/repos?per_page="${porg}")
   for row in $(echo "${repos}" | jq -r '.[] | @base64'); do
     _jq2() {
-      echo ${row} | base64 --decode | jq -r ${1}
+      echo "${row}" | base64 --decode | jq -r "${1}"
     }
     if [[ $(_jq2 '.fork') =~ ^'false'$ ]]; then
       repoAddress=$(_jq2 '.ssh_url')
-      echo ${repoAddress}
-      git clone ${repoAddress}
+      echo "${repoAddress}"
+      git clone "${repoAddress}"
     fi
   done
-  cd project-signer
+  cd project-signer || exit
 fi
