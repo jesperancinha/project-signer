0fd7dd1f91c44ec2d857a5ced1e81713a5053fb7
Joao Esperancinha <jofisaes@gmail.com>
2022-10-20 00:31:03 +0200

    Updates Scripts

--- signAll.sh
+++ signAll.sh
@@ -5,18 +5,18 @@ if [[ -z "$1" ]]; then
 else
   commitMessage=$1
 fi
-echo -e '\e[31m'WARNING!!'\e[0m'
-echo -e '\e[32m'Carefull when using this script. It is ONLY meant to be used as a signing off tool \for ALL repos in your root folder \(..\)'\e[0m'
+echo -e '\e[31mWARNING!!\e[0m'
+echo -e '\e[32mCarefull when using this script. It is ONLY meant to be used as a signing off tool \nfor ALL repos in your root folder \(..\)\e[0m'
 echo -e "This will automatically commit EVERYTHING under ALL your repos with commit message \e[32m\"${commitMessage}\"\e[0m"
 read -p "Are you sure? (Yy/Nn)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   cd ..
   for item in *; do
     if [[ -d "$item" ]] && [[ "$item" != ".git" ]] && [[ "$item" != "target" ]]; then
-      cd "${item}"
+      cd "${item}" || exit
       year="$(git log --reverse | sed -n -e "3,3p" | sed 's/\(.*\)\ \([0-9]*\)\ \(.*\)/\2/')"
-#      find . -iname "License" | xargs sed -i "" 's/\[yyyy\]/'${year}'/g'
-      find . -iname "License" | xargs sed -i'' 's/\[yyyy\]/'${year}'/g'
+      find . -iname "License" | xargs sed -i "" -e 's/\[yyyy\]/'${year}'/g'
+#      find . -iname "License" | xargs -I {} sed -i -e "s/\[yyyy\]/'${year}'/g" {} {}
       echo "----------------- Signing (committing) ${item} -----------------"
       git add .
       if [[ -z "$1" ]]; then
@@ -31,5 +31,5 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
       cd ..
     fi
   done
-  cd project-signer
+  cd project-signer || exit
 fi
--- signYears.sh
+++ signYears.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-echo -e '\e[31m'WARNING!!'\e[0m'
-echo -e '\e[32m'Carefull when using this script. It is ONLY meant to be used as a signing off tool \for ALL repos in your root folder \(..\)'\e[0m'
+echo -e '\e[31mWARNING!!\e[0m'
+echo -e '\e[32mCarefull when using this script. It is ONLY meant to be used as a signing off tool \nfor ALL repos in your root folder \(..\)\e[0m'
 echo -e "This will automatically replace the [yyyy] in the LICENSE files"
 read -p "Are you sure? (Yy/Nn)" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   cd ..
   for item in *; do
     if [[ -d "$item" ]] && [[ "$item" != ".git" ]] && [[ "$item" != "target" ]]; then
-      cd "${item}"
+      cd "${item}" || exit
 
       git pull
       year="$(git log --reverse | sed -n -e "3,3p" | sed 's/\(.*\)\ \([0-9]*\)\ \(.*\)/\2/')"
       echo "----------------- Signing years in repo ${item} -----------------"
       find . -iname "License" | xargs sed -i "" 's/\[yyyy\]/'${year}'/g'
-
+#      find . -iname "License" | xargs -I {} sed -i -e "s/\[yyyy\]/'${year}'/g" {} {}
       cd ..
     fi
   done
-  cd project-signer
+  cd project-signer || exit
 fi
