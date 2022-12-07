7db0f0a25332e4bc3325bf6da473e8d3a23e30c8
Joao Esperancinha <jofisaes@gmail.com>
2022-12-07 19:13:07 +0100

    Update cypress docker-compose

--- extraUpdates.sh
+++ extraUpdates.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cypress=$(cat < "e2e/package.json" | grep cypress | tail -1 | cut -d'"' -f4- | rev | cut -c 2- | rev)
+cypress_docker=$(cat < "e2e/package.json" | grep cypress | tail -1 | cut -d'"' -f4- | cut -c 2- )
 oldcheckout01="actions/checkout@v1"
 oldcheckout02="actions/checkout@v2"
 newcheckout="actions/checkout@v3"
@@ -9,10 +10,32 @@ oldsetupjava02="actions/setup-java@v2"
 newsetupjava="actions/setup-java@v3"
 echo -e "Cypress Version"
 echo -e "--- New version is \e[32m$cypress\e[0m"
+echo -e "--- New docker version is \e[32m$cypress_docker\e[0m"
 echo -e "GitHub Workflow Updates"
 echo -e "--- Update Checkout from \e[32m$oldcheckout01\e[0m to \e[32m$newcheckout\e[0m"
 echo -e "--- Update Checkout from \e[32m$oldcheckout02\e[0m to \e[32m$newcheckout\e[0m"
 echo -e "--- Update Setup Java from \e[32m$oldsetupjava01\e[0m to \e[32m$newsetupjava\e[0m"
 echo -e "--- Update Setup Java from \e[32m$oldsetupjava02\e[0m to \e[32m$newsetupjava\e[0m"
 
-# TODO
+sed -E 's/"cypress": .*/"cypress": "'"$cypress"'"/g' e2e/package.json
+
+REPLY='Y'
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  cd ..
+  for item in *; do
+    if [[ -d "$item" ]] && [[ "$item" != ".git" ]] && [[ "$item" != "target" ]]; then
+      cd "${item}" || exit
+      if [[ -f e2e/package.json ]]; then
+        echo "----------------- Updating Cypress In ${item} -----------------"
+        echo "Cypress from $(cat < "e2e/package.json" | grep -v cypress-pipe | grep cypress | tail -1 | cut -d'"' -f4- | rev | cut -c 2- | rev)"
+        sed -E 's/"cypress": .*"/"cypress": "'"$cypress"'"/g' "e2e/package.json" > e2e/packagetmp.json
+        sed -E 's/image: "cypress\/included:.*"/image: "cypress\/included:'"$cypress_docker"'"/g' "e2e/docker-compose.yml" > e2e/docker-composetmp.yml
+        mv e2e/packagetmp.json e2e/package.json
+        mv e2e/docker-composetmp.yml e2e/docker-compose.yml
+      fi
+      cd ..
+    fi
+  done
+  cd project-signer || exit
+fi
