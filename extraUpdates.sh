37ae707c1c16e0c7f2ce2588a020cfc59f4cb1be
Joao Esperancinha <jofisaes@gmail.com>
2022-12-07 19:38:04 +0100

    Updates checkouts and setup java

--- extraUpdates.sh
+++ extraUpdates.sh
@@ -2,12 +2,12 @@
 
 cypress=$(cat < "e2e/package.json" | grep cypress | tail -1 | cut -d'"' -f4- | rev | cut -c 2- | rev)
 cypress_docker=$(cat < "e2e/package.json" | grep cypress | tail -1 | cut -d'"' -f4- | cut -c 2- )
-oldcheckout01="actions/checkout@v1"
-oldcheckout02="actions/checkout@v2"
-newcheckout="actions/checkout@v3"
-oldsetupjava01="actions/setup-java@v1"
-oldsetupjava02="actions/setup-java@v2"
-newsetupjava="actions/setup-java@v3"
+oldcheckout01="actions\/checkout@v1"
+oldcheckout02="actions\/checkout@v2"
+newcheckout="actions\/checkout@v3"
+oldsetupjava01="actions\/setup-java@v1"
+oldsetupjava02="actions\/setup-java@v2"
+newsetupjava="actions\/setup-java@v3"
 echo -e "Cypress Version"
 echo -e "--- New version is \e[32m$cypress\e[0m"
 echo -e "--- New docker version is \e[32m$cypress_docker\e[0m"
@@ -33,6 +33,19 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         sed -E 's/image: "cypress\/included:.*"/image: "cypress\/included:'"$cypress_docker"'/g' "e2e/docker-compose.yml" > e2e/docker-composetmp.yml
         mv e2e/packagetmp.json e2e/package.json
         mv e2e/docker-composetmp.yml e2e/docker-compose.yml
+        if [[ -d .github/workflows ]]; then
+          rm .github/workflows/*.*01
+          WORKFLOWS=".github/workflows/*"
+          for f in $WORKFLOWS
+          do
+            echo "Processing $f file..."
+            sed -E 's/'$oldcheckout01'/'$newcheckout'/g' "$f" > "$f""01"
+            sed -E 's/'$oldcheckout02'/'$newcheckout'/g' "$f""01" > "$f"
+            sed -E 's/'$oldsetupjava01'/'$newsetupjava'/g' "$f" > "$f""01"
+            sed -E 's/'$oldsetupjava02'/'$newsetupjava'/g' "$f""01" > "$f"
+            rm "$f""01"
+          done
+        fi
       fi
       cd ..
     fi
