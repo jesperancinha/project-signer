e1546bafa957cddd306744b73d9d53c81c0786de
Joao Esperancinha <jofisaes@gmail.com>
2023-01-08 11:12:21 +0100

    Updates docs

--- checkoutMainMaster.sh
+++ checkoutMainMaster.sh
@@ -4,7 +4,7 @@ cd ..
 for item in *; do
   if [[ -d "$item" ]] && [[ "$item" != ".git" ]] && [[ "$item" != "target" ]]; then
     cd "$item" || exit
-    echo "----------------- Checkout/Reverting LICENSE from $item -----------------"
+    echo "----------------- Checkout Main/Master from $item -----------------"
     git checkout main >> /dev/null
     git checkout master
     cd ..
