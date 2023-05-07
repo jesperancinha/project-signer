7d0698219833cb9349b067d051d68c73d4878e75
Joao Esperancinha <jofisaes@gmail.com>
2023-05-07 09:08:46 +0200

    First upgrade project

--- one-offs/replaceGradleUpgradeMakefiles.py
+++ one-offs/replaceGradleUpgradeMakefiles.py
@@ -5,10 +5,15 @@ from os.path import exists
 rootdir = '../../'
 with open("upgradeGradlePattern.txt", "r") as file:
     data = file.read()
+with open("upgradeGradlePatternReplacement.txt", "r") as file2:
+    newData = file2.read()
 for file in os.listdir(rootdir):
     testMakeFile = file + '/Makefile'
     if(exists(os.path.join(rootdir, testMakeFile))):
         makefile = os.path.join(rootdir, testMakeFile)
         with open(makefile) as f:
-            if data in f.read():
+            originalFileContent = f.read()
+            if data in originalFileContent:
                  print(makefile)
+                 with open(makefile, 'w') as f:
+                    f.write(originalFileContent.replace(data, newData))
