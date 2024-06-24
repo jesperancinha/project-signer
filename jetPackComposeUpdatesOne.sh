777d3f6594546fff5feab9f7ef7d1c5058af16cb
João Esperancinha <jofisaes@gmail.com>
2024-06-24 12:18:25 +0200

    plugins update

--- jetPackComposeUpdatesOne.sh
+++ jetPackComposeUpdatesOne.sh
@@ -2,12 +2,19 @@
 
 sudo apt -y install libxml2-utils
 
-newVersion=$(curl -s 'https://dl.google.com/dl/android/maven2/androidx/compose/compiler/compiler/maven-metadata.xml' | xmllint --xpath 'string(//metadata/versioning/latest)' -)
+result=$(curl -s 'https://dl.google.com/dl/android/maven2/androidx/compose/compiler/compiler/maven-metadata.xml')
 
-echo "Updating JetPack compose compiler to version $newVersion"
+echo "Got response $result"
 
-for file in $(find . -name "build.gradle.kts"); do
-  echo "Updating file $file"
-  sed -E 's/kotlinCompilerExtensionVersion = \".*\"/kotlinCompilerExtensionVersion = \"'$newVersion'\"/g' "$file" > "$file""01"
-  mv "$file""01" "$file"
-done
+newVersion=$(echo "$result" | xmllint --xpath 'string(//metadata/versioning/latest)' -)
+
+if [[ -n $newVersion ]]; then
+  echo "Updating JetPack compose compiler to version $newVersion"
+  for file in $(find . -name "build.gradle.kts"); do
+    echo "Updating file $file"
+    sed -E 's/kotlinCompilerExtensionVersion = \".*\"/kotlinCompilerExtensionVersion = \"'$newVersion'\"/g' "$file" > "$file""01"
+    mv "$file""01" "$file"
+  done
+else
+  echo "Failed to retrieve and parse compose version!"
+fi
