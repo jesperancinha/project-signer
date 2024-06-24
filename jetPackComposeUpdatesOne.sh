2b58d508fb67492e1f3e5f6acf71d86775913c04
João Esperancinha <jofisaes@gmail.com>
2024-06-24 12:12:22 +0200

    plugins update

--- jetPackComposeUpdatesOne.sh
+++ jetPackComposeUpdatesOne.sh
@@ -2,7 +2,7 @@
 
 newVersion=$(curl -s 'https://dl.google.com/dl/android/maven2/androidx/compose/compiler/compiler/maven-metadata.xml' | xmllint --xpath 'string(//metadata/versioning/latest)' -)
 
-sudo apt install xmllint
+sudo apt -y install libxml2-utils
 
 echo "Updating JetPack compose compiler to version $newVersion"
 
