97ba7827f9f85279895f61da7d2092c8e71a29e3
João Esperancinha <jofisaes@gmail.com>
2024-06-24 11:36:00 +0200

    update plugins

--- pluginUpdatesOne.sh
+++ pluginUpdatesOne.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+separator="/"
+
 declare -A arr
 declare -A pluginsArr
 
@@ -9,18 +11,25 @@ pluginsArr[2]="actions/setup-node"
 pluginsArr[3]="gradle/gradle-build-action"
 pluginsArr[4]="nick-fields/retry"
 pluginsArr[5]="peter-evans/create-pull-request"
+pluginsArr[6]="dependabot/fetch-metadata"
+pluginsArr[7]="github/codeql-action/init"
 
 for plugin in "${pluginsArr[@]}"; do
-  tag=$(echo $plugin | cut -d'/' -f1)
-  name=$(echo $plugin | cut -d'/' -f2)
+  tag=$(echo $plugin | awk -F"$separator" '{print $1}')
+  name=$(echo $plugin | awk -F"$separator" '{print substr($0, index($0,$2))}')
   echo "$tag"/"$name"
   versionUrl="https://api.github.com/repos/"$tag"/"$name"/tags"
   echo "Performing GET request to $versionUrl"
   result=$(curl -s $versionUrl)
   echo "$result"
+  name=${name//\//\\/}
   version=$( echo "$result" | jq -r '.[0].name' | cut -d '.' -f1)
-  arr["$tag\/$name@v[0-9]*"]="$tag\/$name@$version"
-  echo "Will replace $tag\/$name@v[0-9]* with $tag\/$name@$version"
+  if [[ -n $version ]]; then
+    arr["$tag\/$name@v[0-9\.]*"]="$tag\/$name@$version"
+    echo "Will replace $tag\/$name@v[0-9]* with $tag\/$name@$version"
+  else
+    echo "ERROR! Failed to get $plugin version!"
+  fi
 done
 
 echo -e "GitHub Workflow Updates"
@@ -38,7 +47,7 @@ if [ -d .github/workflows ]; then
       if [[ -n "${arr[${key}]}" ]]; then
         echo -e "--- Updating \e[32m$key\e[0m to \e[32m${arr[${key}]}\e[0m"
         sed -E 's/'$key'/'"${arr[${key}]}"'/g' "$f" > "$f""01"
-        sed -E 's/'$key'/'"${arr[${key}]}"'/g' "$f""01" > "$f"
+        mv "$f""01" "$f"
       fi
     done
     if [ -f "$f""01" ]; then rm "$f""01"; fi
