376bef4221c1f512b686dfc3266d33d57f1d6378
João Esperancinha <jofisaes@gmail.com>
2024-06-24 12:10:23 +0200

    plugins update

---
+++ jetPackComposeUpdatesOne.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+newVersion=$(curl -s 'https://dl.google.com/dl/android/maven2/androidx/compose/compiler/compiler/maven-metadata.xml' | xmllint --xpath 'string(//metadata/versioning/latest)' -)
+
+echo "Updating JetPack compose compiler to version $newVersion"
+
+for file in $(find . -name "build.gradle.kts"); do
+  echo "Updating file $file"
+  sed -E 's/kotlinCompilerExtensionVersion = \".*\"/kotlinCompilerExtensionVersion = \"'$newVersion'\"/g' "$file" > "$file""01"
+  mv "$file""01" "$file"
+done
