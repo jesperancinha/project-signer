bc839a07e0993bc55db7c776f80114bb8150b4fc
João Esperancinha <jofisaes@gmail.com>
2024-06-26 20:59:09 +0200

    nodeUpdate

---
+++ nodeUpdatesOne.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#latestNodeLTS=$(curl -s https://nodejs.org/dist/index.json | jq -r '.[] | select(.lts != false) | .version' | head -n 1 | cut -d'.' -f1 | cut -d 'v' -f2)
+latestNodeLTS="20"
+
+echo "Found node JS version $latestNodeLTS"
+if [[ -n $latestNodeLTS ]]; then
+  #  YML pipeline files and more - Version
+  echo "Scanning for yml files including GitHub action files..."
+  for f in $(find . -name "*.yml"); do
+      sed -E "s/node\-version\: '[0-9]*'/node-version\: '$latestNodeLTS'/g" "$f" > "$f""01"
+      mv "$f""01" "$f"
+      sed -E "s/name\: Use Node\.js\s*[0-9]*/name: Set up Node\.js $latestNodeLTS/g" "$f" > "$f""01"
+      mv "$f""01" "$f"
+      sed -E "s/name\: Set up Node\.js\s*[0-9]*/name: Set up Node\.js $latestNodeLTS/g" "$f" > "$f""01"
+      mv "$f""01" "$f"
+  done
+else
+  echo "Unable to read latest Node LTS version!"
+fi
