abc048aa7113fef44ea6b01eeaf9429967c418c2
João Esperancinha <jofisaes@gmail.com>
2024-06-26 21:09:04 +0200

    test for nonexisting git folder

--- project-signer-runnable/src/test/kotlin/org/jesperancinha/parser/projectsigner/UtilitiesTest.kt
+++ project-signer-runnable/src/test/kotlin/org/jesperancinha/parser/projectsigner/UtilitiesTest.kt
@@ -1,22 +1,30 @@
 package org.jesperancinha.parser.projectsigner
 
 import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.types.shouldBeInstanceOf
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 
 class UtilitiesTest {
 
     @Test
-    fun `should get github username`(){
-        val process = ProcessBuilder("git", "config", "--get", "user.name")
-            .start()
+    fun `should get github username`() {
+        runCatching {
+            val process = ProcessBuilder("git", "config", "--get", "user.name")
+                .start()
 
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val username = reader.readLine()
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            val username = reader.readLine()
 
-        process.waitFor()
+            process.waitFor()
 
-        username shouldBeIn arrayOf("jesperancinha", null, "Joao Esperancinha")
+            username shouldBeIn arrayOf("jesperancinha", null, "Joao Esperancinha")
+        }.onFailure {
+            it.shouldBeInstanceOf<IOException>()
+            it.message.shouldContain("No such file or directory")
+        }
     }
 }
