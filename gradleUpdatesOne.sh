119efa669154e74cae3ede708aec94a9dd641874
jesperancinha <jofisaes@gmail.com>
2024-06-27 13:41:13 +0200

    Adds gradle update

--- gradleUpdatesOne.sh
+++ gradleUpdatesOne.sh
@@ -6,9 +6,16 @@ if [[ -n $latestGradle ]]; then
   #  YML pipeline files and more - Version
   echo "Scanning for yml files including GitHub action files..."
   for f in $(find . -name "*.yml"); do
-      sed -E "s/gradle-version\: [0-9\.[a-z]]/gradle-version: '$latestGradle'/g" "$f" > "$f""01"
+      sed -E "s/gradle-version\: [0-9\.a-z]*/gradle-version: $latestGradle/g" "$f" > "$f""01"
       mv "$f""01" "$f"
   done
+  #  Makefile Gradle parent GRADLE_VERSION
+  echo "Scanning Makefile"
+  f="Makefile"
+  if [ -f $f ]; then
+    sed -E "s/GRADLE_VERSION \?= [0-9\.a-z]*/GRADLE_VERSION ?= $latestGradle/g" "$f" > "$f""01"
+    mv "$f""01" "$f"
+  fi
   ./gradlew build
 else
   echo "Unable to read latest Java LTS version!"
