0a797771e5d2f5650da4f03698bd27434502df13
João Esperancinha <jofisaes@gmail.com>
2024-07-07 09:28:11 +0200

    test

--- acceptPR.sh
+++ acceptPR.sh
@@ -2,8 +2,22 @@
 echo "Used only to create an extra commit so that PR's created with the plugin get started"
 echo "More info one:"
 echo "- https://github.com/orgs/community/discussions/25702"
+
+function echoPRDate() {
+  "PR Accept on $(date)" >> pr.md
+}
+
 LANG=en_us_8859_1
-echo "PR Accept on $(date)" >> pr.md
-git add pr.md
-git commit -m "Accepts Automated PRs on $(date)" -- pr.md
+if [ ! -f pr.md ]; then
+  touch pr.md
+  git pr.md
+  echoPRDate
+  git add pr.md
+  git commit -m "Accepts Automated PRs on $(date)"
+else
+  echoPRDate
+  git add pr.md
+  git commit -m "Accepts Automated PRs on $(date)" -- pr.md
+fi
+
 git push
