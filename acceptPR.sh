71b281327df052106d612756306241142afedf0d
João Esperancinha <jofisaes@gmail.com>
2024-07-07 09:19:57 +0200

    acceptPR script

--- acceptPR.sh
+++ acceptPR.sh
@@ -5,3 +5,4 @@ echo "- https://github.com/orgs/community/discussions/25702"
 LANG=en_us_8859_1
 echo "PR Accept on $(date)" >> pr.md
 git commit -m "Accepts Automated PRs on $(date)" -- pr.md
+git push
