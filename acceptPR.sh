b854ce32284f3e6b5a30858c441e16f8e41103ab
João Esperancinha <jofisaes@gmail.com>
2024-07-07 09:19:29 +0200

    acceptPR script

--- acceptPR.sh
+++ acceptPR.sh
@@ -2,4 +2,6 @@
 echo "Used only to create an extra commit so that PR's created with the plugin get started"
 echo "More info one:"
 echo "- https://github.com/orgs/community/discussions/25702"
-LANG=en_us_8859_1 && echo "PR Accept on $(shell date)" >> pr.md
+LANG=en_us_8859_1
+echo "PR Accept on $(date)" >> pr.md
+git commit -m "Accepts Automated PRs on $(date)" -- pr.md
---
+++ pr.md
@@ -0,0 +1,1 @@
+PR Accept on Sun Jul  7 09:19:10 CEST 2024
