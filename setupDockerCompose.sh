92b1174eb833ff8f61bda083084355c2216d6fa9
jesperancinha <jofisaes@gmail.com>
2024-08-05 19:01:03 +0200

    docker-compose alternatives

--- setupDockerCompose.sh
+++ setupDockerCompose.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env sh
+echo "$(tput setaf 2)Starting upgrade from docker-compose version $(docker-compose -v | tail -n 1)"; \
+echo "If DOCKER_COMPOSE_VERSION env variable is not given, this upgrade will install the latest version"; \
+if [ -z "$DOCKER_COMPOSE_VERSION" ]; then \
+    DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4); \
+else \
+    echo "$(tput setaf 1)DOCKER_COMPOSE_VERSION not given. Upgrading to latest available version..."; \
+fi; \
+echo "$(tput setaf 2)Installing version $DOCKER_COMPOSE_VERSION of docker-compose"; \
+echo "$(tput setaf 7)"; \
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}; \
+mkdir -p $DOCKER_CONFIG/cli-plugins; \
+curl -SL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose; \
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose;
 if [ ! -f /usr/bin/docker-compose ]; then
-  curl -O https://raw.githubusercontent.com/jesperancinha/project-signer/master/docker-compose;
-  sudo chmod +x docker-compose;
-  sudo cp docker-compose /usr/bin/docker-compose;
+  ln -s ~/.docker/cli-plugins/docker-compose /usr/bin/docker-compose
 else
-  echo "File already exists! Continuing";
+  echo "File already exists! Continuing"
 fi
