3bd709787eadd0a7bc0952431b747a702cb14fdf
jesperancinha <jofisaes@gmail.com>
2024-08-05 15:38:43 +0200

    docker-compose alternatives

--- setupDockerCompose.sh
+++ setupDockerCompose.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-if [ ! -f /usr/local/bin/docker-compose ]; then
+if [ ! -f /usr/bin/docker-compose ]; then
   curl -O https://raw.githubusercontent.com/jesperancinha/project-signer/master/docker-compose
-  sudo cp docker-compose /usr/local/bin/docker-compose;
+  sudo cp docker-compose /usr/bin/docker-compose;
 else
   echo "File already exists! Continuing"
 fi
