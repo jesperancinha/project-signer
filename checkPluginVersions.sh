3fee413d85e844f716fbdc734ef2abfbce87c0f9
jesperancinha <jofisaes@gmail.com>
2024-09-08 10:10:28 +0200

    Plugin updates care

--- checkPluginVersions.sh
+++ checkPluginVersions.sh
@@ -64,18 +64,3 @@ for key in "${!arr[@]}"; do
     echo -e "--- Update \e[32m$key\e[0m to \e[32m${arr[${key}]}\e[0m"
   fi
 done
-if [ -d .github/workflows ]; then
-  WORKFLOWS=".github/workflows/*"
-  for f in $WORKFLOWS
-  do
-    echo "Processing $f file..."
-    for key in "${!arr[@]}"; do
-      if [[ -n "${arr[${key}]}" ]]; then
-        echo -e "--- Updating \e[32m$key\e[0m to \e[32m${arr[${key}]}\e[0m"
-        sed -E 's/'$key'/'"${arr[${key}]}"'/g' "$f" > "$f""01"
-        mv "$f""01" "$f"
-      fi
-    done
-    if [ -f "$f""01" ]; then rm "$f""01"; fi
-  done
-fi
