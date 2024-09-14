155d6e35f83195c9b92138d31bd47b99bb17d00f
jesperancinha <jofisaes@gmail.com>
2024-09-14 15:38:16 +0200

    Removes useless badge

--- one-offs/badges/elmuxRemover.sh
+++ one-offs/badges/elmuxRemover.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-f="Readme.md"
 cd ../../..
 for item in *; do
   if [[ -d "$item" ]] && [[ "$item" != ".git" ]] && [[ "$item" != "target" ]]; then
     cd "$item" || exit
     echo "----------------- Removing Badge on project $item -----------------"
-    if [ -f Readme.md ]; then
-      sed -E "s/\[!\[Twitter URL](.*)//g" "$f" > "$f""01"
-      mv "$f""01" "$f"
-    fi
+    find . -name "Readme.md" | while IFS= read -r readme; do
+      sed -E "s/\[!\[Twitter URL](.*)//g" "$readme" > "$readme""01"
+      mv "$readme""01" "$readme"
+    done
     cd ..
   fi
 done
