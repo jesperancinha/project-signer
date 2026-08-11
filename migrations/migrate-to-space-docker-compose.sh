88e84239bdd5aea869ec78f00ce8257bace3ec11
jesperancinha <jofisaes@gmail.com>
2026-08-11 14:20:55 +0200

    Updates docker usage

--- migrations/migrate-to-space-docker-compose.sh
+++ migrations/migrate-to-space-docker-compose.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
-SEARCH='docker compose '
+SEARCH='docker-compose '
 REPLACE='docker compose '
+SEARCH2='docker-compose.yml'
+REPLACE2='docker-compose.yml'
 
-find .. -type f \( \
+find ../.. -type f \( \
     -name '*.sh' -o \
     -name 'Makefile' -o \
     -name 'Makefile.mk' -o \
@@ -16,5 +18,9 @@ while IFS= read -r -d '' file; do
     if grep -qF "$SEARCH" "$file"; then
         echo "Updating: $file"
         sed -i "s/${SEARCH}/${REPLACE}/g" "$file"
+    fi
+    if grep -qF "$SEARCH2" "$file"; then
+        echo "Updating: $file"
+        sed -i "s/${SEARCH2}/${REPLACE2}/g" "$file"
     fi
 done
