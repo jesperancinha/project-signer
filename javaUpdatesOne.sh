1f97759d0b82b800026ff07a202d4a68cea33bf7
jesperancinha <jofisaes@gmail.com>
2026-08-13 10:20:46 +0200

    Updates script for Java Dockerfile

--- javaUpdatesOne.sh
+++ javaUpdatesOne.sh
@@ -48,7 +48,7 @@ if [[ -n $latestJavaLTS ]]; then
       mv "$f""01" "$f"
       sed -E 's/FROM .*jre.*/FROM '"$targetImage"'/g' "$f" > "$f""01"
       mv "$f""01" "$f"
-      sed -E 's/FROM .*eclipse-temurin.*/FROM '"$targetImage"'/g' "$f" > "$f""01"
+      sed -E 's/FROM .*eclipse\-temurin.*/FROM '"$targetImage"'/g' "$f" > "$f""01"
       mv "$f""01" "$f"
   done
 
